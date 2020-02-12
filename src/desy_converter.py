c54d449b39639bf161d134735ac7cb33238d4429
michareichmann <micha.reichmann@gmail.com>
2020-02-12 18:19:22 +0100

    revision of desy converter with proteus

--- src/desy_converter.py
+++ src/desy_converter.py
@@ -5,17 +5,15 @@
 # --------------------------------------------------------
 from __future__ import print_function
 
-from utils import *
+from draw import *
+from ROOT import TBrowser
 from os import chdir, remove
-from os.path import basename, join, dirname
+from os.path import basename, join
 from argparse import ArgumentParser
-from collections import OrderedDict
-from progressbar import Bar, ETA, FileTransferSpeed, Percentage, ProgressBar
-from pickle import load, dump
-from draw import Draw, ufloat
 from converter import Converter
 from subprocess import check_call
 from glob import glob
+from numpy import concatenate
 
 
 class DESYConverter(Converter):
@@ -29,6 +27,38 @@ class DESYConverter(Converter):
         self.SaveDir = join(self.TCDir, 'hdf5')
         self.ROOTFileName = '{}.root'.format(basename(self.RawFilePath).split('_')[0])
 
+        self.AlignSteps = self.get_align_steps()
+        self.Draw = Draw()
+
+        self.B = []
+
+    def get_z_positions(self):
+        with open(join(self.ProteusDataDir, 'geometry.toml')) as f:
+            return array([float(line.split(',')[-1].strip(']\n\r')) for line in f.readlines() if line.startswith('offset')])
+
+    def get_align_steps(self):
+        with open(join(self.ProteusDataDir, 'analysis.toml')) as f:
+            return [line.split(']')[0].replace('[align.', '') for line in f.readlines() if line.startswith('[align.')]
+
+    def draw_track(self, ev=10):
+        from ROOT import TFile
+        from draw import Draw, format_histo
+        d = Draw()
+        f = TFile(join(self.SaveDir, self.ROOTFileName))
+        trees = [f.Get(key.GetName()).Get('Hits') for key in f.GetListOfKeys() if 'Plane' in key.GetName()]
+        for tree in trees:
+            tree.GetEntry(ev)
+        x, y, n = [array([array(getattr(tree, att)) for tree in trees]) for att in ['PixX', 'PixY', 'NHits']]
+        zed = concatenate([[i_z] * i_n for i_z, i_n in zip(self.get_z_positions(), n)])
+        sx = concatenate([([.0184] if i < 6 else [.15]) * i_n for i, i_n in enumerate(n)])
+        sy = concatenate([([.0184] if i < 6 else [.1]) * i_n for i, i_n in enumerate(n)])
+        gx = d.make_tgrapherrors('gx', 'x', x=zed, y=concatenate(x) * sx)
+        gy = d.make_tgrapherrors('gy', 'y', x=zed, y=concatenate(y) * sy)
+        for g in [gx, gy]:
+            format_histo(g, x_tit='z [mm]')
+            d.draw_histo(g, draw_opt='ap')
+        self.B.append([trees, f, d])
+
     def convert_raw_to_root(self):
         ensure_dir(self.SaveDir)
         chdir(self.SaveDir)
@@ -38,11 +68,11 @@ class DESYConverter(Converter):
         for filename in glob('AutoDict_vector*'):
             remove(filename)
 
-    def run_proteus(self, prog, out, geo=None, sub_section=None):
+    def run_proteus(self, prog, out, geo=None, sub_section=None, f=None):
         chdir(self.ProteusDataDir)
         sub_section = '' if sub_section is None else ' -u {}'.format(sub_section)
         geo = '' if geo is None else ' -g {}'.format(geo)
-        cmd = '{} {} {}{}{}'.format(join(self.ProteusSoftDir, 'bin', prog), join(self.SaveDir, self.ROOTFileName), out, geo, sub_section)
+        cmd = '{} {} {}{}{}'.format(join(self.ProteusSoftDir, 'bin', prog), join(self.SaveDir, self.ROOTFileName) if f is None else f, out, geo, sub_section)
         info(cmd)
         check_call(cmd.split())
 
@@ -52,22 +82,33 @@ class DESYConverter(Converter):
 
     def align(self):
         ensure_dir('align')
-        self.align_step('tel_coarse')
-        self.align_step('dut_coarse', 'tel_coarse')
-        self.align_step('tel_fine', 'dut_coarse')
-        self.align_step('dut_fine', 'tel_fine')
+        for i in xrange(len(self.AlignSteps)):
+            self.align_step(i)
 
-    def align_step(self, sub_section, geo=None):
-        if not file_exists(make_geo(sub_section)):
-            self.run_proteus('pt-align', join('align', sub_section), make_geo(geo), sub_section)
+    def align_step(self, step, force=False):
+        step_name = self.AlignSteps[step]
+        if not file_exists(self.make_toml_name(step_name)) or force:
+            self.run_proteus('pt-align', join('align', step_name), self.make_toml_name(self.AlignSteps[step - 1] if step else None), step_name)
         else:
             warning('geo file already exists!')
 
     def track(self):
-        self.run_proteus('pt-track', 'bla', make_geo('dut_fine'))
+        self.run_proteus('pt-track', 'track', self.make_toml_name())
 
-def make_geo(name, d='align'):
-    return None if name is None else join(d, '{}-geo.toml'.format(name))
+    def match(self):
+        self.run_proteus('pt-match', 'match', self.make_toml_name(), f='track-data.root')
+
+    def check_alignment(self, step=0, p1=0, p2=1):
+        f = TFile(join(self.ProteusDataDir, 'align', '{}-hists.root'.format(self.AlignSteps[step])))
+        tdir = f.Get('validation').Get('correlations').Get('M{}-M{}'.format(p1, p2))
+        c = self.Draw.make_canvas('c_cor{}'.format(step), 'Correlations Plane {} & {}'.format(p1, p2), divide=(2, 2))
+        self.Draw.format_statbox(all_stat=True)
+        for i, h in enumerate([tdir.Get(name) for name in ['correlation_x', 'correlation_y', 'difference_x', 'difference_y']], 1):
+            self.Draw.draw_histo(h, canvas=c.cd(i), draw_opt='colz' if i in [1, 2] else '')
+        self.Draw.Objects.append([f])
+
+    def make_toml_name(self, name=None, d='align', typ='geo'):
+        return join(d, '{}-{}.toml'.format(self.AlignSteps[-1] if name is None else name, typ))
 
 
 if __name__ == '__main__':
@@ -75,7 +116,7 @@ if __name__ == '__main__':
     from desy_run import DESYRun
 
     p = ArgumentParser()
-    p.add_argument('run', nargs='?', default='')
+    p.add_argument('run', nargs='?', default=11)
     # noinspection PyTypeChecker
     p.add_argument('dut', nargs='?', default=1, type=int)
     args = p.parse_args()
