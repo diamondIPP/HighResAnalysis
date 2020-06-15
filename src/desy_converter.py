62ec14cac36cbdfd0914198a2473ca3186d51bbc
michareichmann <micha.reichmann@gmail.com>
2020-06-15 18:59:42 +0200

    complete converte desy raw file to hd5f

--- src/desy_converter.py
+++ src/desy_converter.py
@@ -6,7 +6,6 @@
 from __future__ import print_function
 
 from draw import *
-from ROOT import TBrowser
 from os import chdir, remove
 from os.path import basename, join
 from argparse import ArgumentParser
@@ -17,21 +16,70 @@ from numpy import concatenate
 
 
 class DESYConverter(Converter):
+    """ Converts EUDAQ2 raw files in several steps into hdf5 files.
+        STEP 0: raw -> root     (EUDAQ2)
+        STEP 1: noisescan       (proteus)
+        STEP 2: alignment       (proteus)
+        STEP 3: tracking        (proteus)
+        STEP 4: track-matching  (proteus)
+        STEP 5: root -> hdf5    (python) """
 
-    def __init__(self, filepath, config):
-        Converter.__init__(self, filepath, config)
+    def __init__(self, data_dir, run_number, config):
+        Converter.__init__(self, data_dir, run_number, config)
 
+        # DIRECTORIES
         self.EUDAQDir = join(self.SoftDir, self.Config.get('SOFTWARE', 'eudaq2'))
         self.ProteusSoftDir = join(self.SoftDir, self.Config.get('SOFTWARE', 'proteus'))
-        self.ProteusDataDir = join(self.TCDir, 'proteus')
-        self.SaveDir = join(self.TCDir, 'hdf5')
-        self.ROOTFileName = '{}.root'.format(basename(self.RawFilePath).split('_')[0])
+        self.ProteusDataDir = join(self.DataDir, 'proteus')
+        self.SaveDir = join(self.DataDir, 'data')
 
+        # FILENAMES
+        self.ROOTFileName = join(self.SaveDir, 'run{:06d}.root'.format(self.RunNumber))
+        self.NoiseName = join('mask', 'all')
         self.AlignSteps = self.get_align_steps()
+        self.ProteusROOTFiles = [join(self.ProteusDataDir, '{}.root'.format(n)) for n in ['track-data', 'match-trees']]
+        self.FinalFileName = join(self.SaveDir, 'run{:04d}.hdf5'.format(self.RunNumber))
+        self.FileNames = [self.ROOTFileName] + [self.get_toml_path(p) for p in [self.NoiseName, join('align', self.AlignSteps[-1])]] + self.ProteusROOTFiles + [self.FinalFileName]
+
+        self.NSteps = len(self.FileNames)
+        self.AtStep = self.find_step()
+
         self.Draw = Draw()
 
-        self.B = []
+    # TODO: add check for alignment or align every run?
 
+    def run(self):
+        for f in self.get_steps():
+            print_banner('Starting{}'.format(f.__doc__))
+            f()
+
+    # ----------------------------------------
+    # region INIT
+    def load_raw_file_name(self):
+        names = glob(join(self.DataDir, 'raw', 'run{:06d}*.raw'.format(self.RunNumber)))
+        return names[0] if names else None
+
+    def find_step(self):
+        for i in reversed(range(self.NSteps)):
+            if file_exists(self.FileNames[i]):
+                return i + 1
+        return 0
+
+    def get_steps(self):
+        steps = [self.convert_raw_to_root, self.noise_scan, self.align, self.track, self.match, self.convert_root_to_hdf5]
+        return steps[self.AtStep:]
+
+    @staticmethod
+    def check_root_version():
+        version = gROOT.GetVersion()
+        if not version.startswith('6'):
+            warning('you require ROOT6 to run proteus! Current version: {}'.format(version))
+        return version.startswith('6')
+    # endregion INIT
+    # ----------------------------------------
+
+    # ----------------------------------------
+    # region GET
     def get_z_positions(self):
         with open(join(self.ProteusDataDir, 'geometry.toml')) as f:
             return array([float(line.split(',')[-1].strip(']\n\r')) for line in f.readlines() if line.startswith('offset')])
@@ -39,6 +87,132 @@ class DESYConverter(Converter):
     def get_align_steps(self):
         with open(join(self.ProteusDataDir, 'analysis.toml')) as f:
             return [line.split(']')[0].replace('[align.', '') for line in f.readlines() if line.startswith('[align.')]
+
+    def get_toml_path(self, path):
+        name = basename(path)
+        d = None if dirname(path) == name else dirname(path)
+        return join(self.ProteusDataDir, self.make_toml_name(name, d, typ='geo' if d is None else d))
+    # endregion GET
+    # ----------------------------------------
+
+    def convert_raw_to_root(self):
+        """ step 0: convert raw file to root file with eudaq"""
+        if self.RawFilePath is None:
+            critical('raw file does not for run: {}'.format(self.RunNumber))
+        ensure_dir(self.SaveDir)
+        chdir(self.SaveDir)
+        cmd = '{} -i {} -o {}'.format(join(self.EUDAQDir, 'bin', 'euCliConverter'), self.RawFilePath, self.ROOTFileName)
+        info('Convert {} to {}.root using eudaq-2\n'.format(basename(self.RawFilePath), self.ROOTFileName))
+        check_call(cmd.split())
+        for filename in glob('AutoDict_vector*'):
+            remove(filename)
+
+    def get_tree(self, plane=0):
+        root_file = TFile(self.FileNames[-3])
+        return root_file, root_file.Get('Plane{}'.format(plane))
+
+    def convert_root_to_hdf5(self):
+        """ step 5: convert root file to hdf5 file. """
+        start_time = info('Start root->hdf5 conversion ...')
+        root_file = TFile(self.FileNames[-3])
+        if file_exists(self.FinalFileName):
+            remove_file(self.FinalFileName)
+        f = h5py.File(self.FinalFileName, 'w')
+
+        # tracks
+        t = root_file.Get('Tracks')
+        names = [n.GetName() for n in t.GetListOfBranches()]  # NTracks, Chi2, Dof, X, Y, SlopeX, SlopeY, Cov
+        f.create_dataset('NTracks', data=get_root_vec(t, var=names[0], dtype='u1'))
+        n = t.Draw(':'.join(names[1:-1]), '', 'goff')
+        types = ['f2', 'u1', 'f2', 'f2', 'f2', 'f2']
+        for i, (name, typ) in enumerate(zip(names[1:-1], types)):
+            f.create_dataset(name, data=get_root_vec(t, n, i, dtype=typ))
+
+        # planes
+        names = [n.GetName() for n in root_file.GetListOfKeys() if n.GetName().startswith('Plane')]
+        info('reading branches for {} planes'.format(len(names)))
+        hit_types = {'X': 'u2', 'Y': 'u2', 'ADC': 'u1', 'HitInCluster': 'u1'}
+        plane_types = {'X': 'f4', 'Y': 'f4', 'VarX': 'f2', 'VarY': 'f2', 'NTracks': 'u1'}
+        intercept_types = {'X': 'f4', 'Y': 'f4', 'SlopeX': 'f2', 'SlopeY': 'f2', 'NTracks': 'u1'}
+        self.PBar.start(sum(len(lst) for lst in [hit_types, plane_types, intercept_types]) * len(names))
+        for i, name in enumerate(names):
+            g = f.create_group(name)
+
+            # hits: NHits, PixX, PixY, Timing, Value, HitInCluster
+            self.convert_plane_data(root_file, g, name, branch_name='Hits', types=hit_types)
+
+            # clusters:  NClusters, Col, Row, VarCol, VarRow, CovColRow, Timing, Value, Track
+            self.convert_plane_data(root_file, g, name, 'Clusters', plane_types, exclude='Value')  # adc doesn't make sense for clusters
+
+            # intercepts
+            self.convert_plane_data(root_file, g, name, 'Intercepts', intercept_types)  # adc doesn't make sense for clusters
+
+        add_to_info(start_time, 'Finished conversion in')
+
+    def convert_plane_data(self, root_file, group, plane_name, branch_name, types, exclude=None):
+        g0 = group.create_group(branch_name)
+        t0 = root_file.Get(plane_name).Get(branch_name)  # NHits, PixX, PixY, Timing, Value, HitInCluster
+        n_name = 'N{}'.format(branch_name)
+        g0.create_dataset(n_name, data=get_root_vec(t0, var=n_name, dtype='u2'))
+        exclude = make_list(exclude, dtype=list) + ['Timing', 'Cov', 'CovColRow']  # Timing & CovColRow is empty, Cov too much data
+        names = [b.GetName() for b in t0.GetListOfBranches() if b.GetName() not in exclude][1:]
+        n = t0.Draw(':'.join(names), '', 'goff')
+        for j, (branch_name, typ) in enumerate(types.items()):
+            data = get_root_vec(t0, n, j, typ)
+            self.PBar.update()
+            if branch_name == 'ADC' and mean(data) == 1:  # don't save empty data... not all planes have pulse height information
+                continue
+            g0.create_dataset(branch_name, data=data)
+
+
+
+    def run_proteus(self, prog, out, geo=None, sub_section=None, f=None):
+        if not self.check_root_version():
+            return
+        chdir(self.ProteusDataDir)
+        sub_section = '' if sub_section is None else ' -u {}'.format(sub_section)
+        geo = '' if geo is None else ' -g {}'.format(geo)
+        cmd = '{} {} {}{}{}'.format(join(self.ProteusSoftDir, 'bin', prog), join(self.SaveDir, self.ROOTFileName) if f is None else f, out, geo, sub_section)
+        info(cmd)
+        check_call(cmd.split())
+
+    def noise_scan(self):
+        """ step 1: find noisy pixels with proteus. """
+        ensure_dir('mask')
+        self.run_proteus('pt-noisescan', self.NoiseName)
+
+    def align(self):
+        """ step 2: align the telescope in several steps with proteus. """
+        ensure_dir('align')
+        for i in range(len(self.AlignSteps)):
+            self.align_step(i)
+
+    def align_step(self, step, force=False):
+        step_name = self.AlignSteps[step]
+        if not file_exists(self.make_toml_name(step_name)) or force:
+            self.run_proteus('pt-align', join('align', step_name), self.make_toml_name(self.AlignSteps[step - 1] if step else None), step_name)
+        else:
+            warning('geo file already exists!')
+
+    def track(self):
+        """ step 3: based on the alignment generate the tracks with proteus. """
+        self.run_proteus('pt-track', 'track', self.make_toml_name())
+
+    def match(self):
+        """ step 4: match the tracks to the hits in the DUT with proteus. """
+        self.run_proteus('pt-match', 'match', self.make_toml_name(), f='track-data.root')
+
+    def check_alignment(self, step=0, p1=0, p2=1):
+        f = TFile(join(self.ProteusDataDir, 'align', '{}-hists.root'.format(self.AlignSteps[step])))
+        tdir = f.Get('validation').Get('correlations').Get('M{}-M{}'.format(p1, p2))
+        c = self.Draw.make_canvas('c_cor{}'.format(step), 'Correlations Plane {} & {}'.format(p1, p2), divide=(2, 2))
+        self.Draw.format_statbox(all_stat=True)
+        for i, h in enumerate([tdir.Get(name) for name in ['correlation_x', 'correlation_y', 'difference_x', 'difference_y']], 1):
+            self.Draw.draw_histo(h, canvas=c.cd(i), draw_opt='colz' if i in [1, 2] else '')
+        self.Draw.Objects.append([f])
+
+    def make_toml_name(self, name=None, d='align', typ='geo'):
+        return join(d, '{}-{}.toml'.format(self.AlignSteps[-1] if name is None else name, typ))
 
     def draw_track(self, ev=10):
         from ROOT import TFile
@@ -57,70 +231,19 @@ class DESYConverter(Converter):
         for g in [gx, gy]:
             format_histo(g, x_tit='z [mm]')
             d.draw_histo(g, draw_opt='ap')
-        self.B.append([trees, f, d])
-
-    def convert_raw_to_root(self):
-        ensure_dir(self.SaveDir)
-        chdir(self.SaveDir)
-        cmd = '{} -i {} -o {}'.format(join(self.EUDAQDir, 'bin', 'euCliConverter'), self.RawFilePath, self.ROOTFileName)
-        info('Convert {} to {}.root using eudaq-2\n'.format(basename(self.RawFilePath), self.ROOTFileName))
-        check_call(cmd.split())
-        for filename in glob('AutoDict_vector*'):
-            remove(filename)
-
-    def run_proteus(self, prog, out, geo=None, sub_section=None, f=None):
-        chdir(self.ProteusDataDir)
-        sub_section = '' if sub_section is None else ' -u {}'.format(sub_section)
-        geo = '' if geo is None else ' -g {}'.format(geo)
-        cmd = '{} {} {}{}{}'.format(join(self.ProteusSoftDir, 'bin', prog), join(self.SaveDir, self.ROOTFileName) if f is None else f, out, geo, sub_section)
-        info(cmd)
-        check_call(cmd.split())
-
-    def noise_scan(self):
-        ensure_dir('mask')
-        self.run_proteus('pt-noisescan', join('mask', 'all'))
-
-    def align(self):
-        ensure_dir('align')
-        for i in xrange(len(self.AlignSteps)):
-            self.align_step(i)
-
-    def align_step(self, step, force=False):
-        step_name = self.AlignSteps[step]
-        if not file_exists(self.make_toml_name(step_name)) or force:
-            self.run_proteus('pt-align', join('align', step_name), self.make_toml_name(self.AlignSteps[step - 1] if step else None), step_name)
-        else:
-            warning('geo file already exists!')
-
-    def track(self):
-        self.run_proteus('pt-track', 'track', self.make_toml_name())
-
-    def match(self):
-        self.run_proteus('pt-match', 'match', self.make_toml_name(), f='track-data.root')
-
-    def check_alignment(self, step=0, p1=0, p2=1):
-        f = TFile(join(self.ProteusDataDir, 'align', '{}-hists.root'.format(self.AlignSteps[step])))
-        tdir = f.Get('validation').Get('correlations').Get('M{}-M{}'.format(p1, p2))
-        c = self.Draw.make_canvas('c_cor{}'.format(step), 'Correlations Plane {} & {}'.format(p1, p2), divide=(2, 2))
-        self.Draw.format_statbox(all_stat=True)
-        for i, h in enumerate([tdir.Get(name) for name in ['correlation_x', 'correlation_y', 'difference_x', 'difference_y']], 1):
-            self.Draw.draw_histo(h, canvas=c.cd(i), draw_opt='colz' if i in [1, 2] else '')
-        self.Draw.Objects.append([f])
-
-    def make_toml_name(self, name=None, d='align', typ='geo'):
-        return join(d, '{}-{}.toml'.format(self.AlignSteps[-1] if name is None else name, typ))
+        self.Draw.add(trees, f, d)
 
 
 if __name__ == '__main__':
     from analysis import Analysis
     from desy_run import DESYRun
 
-    p = ArgumentParser()
-    p.add_argument('run', nargs='?', default=11)
+    parser = ArgumentParser()
+    parser.add_argument('run', nargs='?', default=11)
     # noinspection PyTypeChecker
-    p.add_argument('dut', nargs='?', default=1, type=int)
-    args = p.parse_args()
+    parser.add_argument('dut', nargs='?', default=1, type=int)
+    pargs = parser.parse_args()
     a = Analysis()
-    r = DESYRun(args.run, args.dut, a.TCDir, a.Config, single_mode=True)
-    z = DESYConverter(r.RawFileName, a.Config)
+    r = DESYRun(pargs.run, pargs.dut, a.TCDir, a.Config, single_mode=True)
+    z = DESYConverter(r.TCDir, r.Number, a.Config)
     # z.run()
