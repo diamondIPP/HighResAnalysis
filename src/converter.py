14d903c119159145c0b14f15ef08951a0da9d273
michareichmann <micha.reichmann@gmail.com>
2020-07-08 16:22:36 +0200

    move Cluster class from desy-converter to base converter

--- src/converter.py
+++ src/converter.py
@@ -6,17 +6,16 @@
 from utils import *
 from os.path import expanduser
 from argparse import ArgumentParser
-from numpy import average
+from numpy import average, sum, append
 
 
 class Converter:
 
-    def __init__(self, data_dir, run_number, config, calibration):
+    def __init__(self, data_dir, run_number, config):
 
         self.RunNumber = run_number
         self.DataDir = data_dir
         self.Config = config
-        self.Calibration = calibration
 
         self.RawFilePath = self.load_raw_file_name()
         self.SoftDir = expanduser(self.Config.get('SOFTWARE', 'dir'))
@@ -47,30 +46,30 @@ class Hit:
 
 class Cluster:
 
-    def __init__(self):
-
-        self.Hits = []
-
-    def __str__(self):
-        return 'Cluster of size {}, Charge: {}'.format(self.size(), self.charge())
+    def __init__(self, seed_hit):
+        self.Hits = array([seed_hit])
 
     def add_hit(self, hit):
-        self.Hits.append(hit)
+        self.Hits = append(self.Hits, [hit], axis=0)
 
-    def size(self):
-        return len(self.Hits)
+    def hit_is_adjacent(self, hit):
+        """ returns: if any of the existing cluster hits has a distance of 1 to the given hit"""
+        return any(sqrt(sum(abs(hit[:2] - self.Hits[:, :2]), axis=1)) <= 1.1)
 
-    def charge(self):
-        return sum(hit.Charge for hit in self.Hits)
+    def get_charge(self):
+        return sum(self.Hits[:, 2])
 
-    def seed_hit(self):
-        return max(self.Hits, key=lambda hit: hit.Charge)
+    def get_size(self):
+        return self.Hits.shape[0]
 
-    def x(self):
-        return average([hit.X for hit in self.Hits], weights=[hit.Charge for hit in self.Hits])
+    def get_x(self):
+        return average(self.Hits[:, 0], weights=self.Hits[:, 2])
 
-    def y(self):
-        return average([hit.Y for hit in self.Hits], weights=[hit.Charge for hit in self.Hits])
+    def get_y(self):
+        return average(self.Hits[:, 1], weights=self.Hits[:, 2])
+
+    def __repr__(self):
+        return 'Cluster with {} hits at [{:.1f}, {:.1f}] with charge {:.1f} vcals'.format(self.get_size(), self.get_x(), self.get_y(), self.get_charge())
 
 
 if __name__ == '__main__':
@@ -79,5 +78,5 @@ if __name__ == '__main__':
     # noinspection PyTypeChecker
     p.add_argument('plane', nargs='?', default=0, type=int)
     args = p.parse_args()
-    z = Converter('/scratch2/cern/2018-10/cms-raw/ljutel_110.root', 0, '/scratch2/cern/2018-10/II6-B6', None)
+    z = Converter('/scratch2/cern/2018-10/cms-raw/ljutel_110.root', 0, '/scratch2/cern/2018-10/II6-B6')
     # z.run()
