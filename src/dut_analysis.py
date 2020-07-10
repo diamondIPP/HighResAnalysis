6edb14d4e3530e57c2cb92a55136d923cfa31e5b
michareichmann <micha.reichmann@gmail.com>
2020-07-10 14:18:30 +0200

    make all analysis methods work

--- src/dut_analysis.py
+++ src/dut_analysis.py
@@ -3,7 +3,7 @@
 #       small script to read simple text files written by pXar
 # created on August 30th 2018 by M. Reichmann (remichae@phys.ethz.ch)
 # --------------------------------------------------------
-from ROOT import TH1I, TProfile2D, TCut, TProfile, TH1F, TH2F
+from ROOT import TProfile2D, TH1F, TH2F
 from cern_run import CERNRun
 from desy_run import DESYRun
 from currents import Currents
@@ -14,7 +14,7 @@ import bins
 from tracks import TrackAnalysis
 from telescope import TelescopeAnalysis
 from analysis import *
-from numpy import in1d
+from numpy import in1d, cumsum, split, concatenate, unique
 from calibration import Calibration
 
 
@@ -59,11 +59,11 @@ class DUTAnalysis(Analysis):
 
     def load_file(self):
         self.Converter.run()
-        f = h5py.File(self.Run.FileName, 'r')
         try:
+            f = h5py.File(self.Run.FileName, 'r')
             _ = f['NTracks']
             return f
-        except KeyError:
+        except (KeyError, OSError):
             warning('could not load data file {} -> start with dummy'.format(self.Run.FileName))
             return {'NTracks': array([])}  # dummy
 
@@ -74,21 +74,52 @@ class DUTAnalysis(Analysis):
 
     # ----------------------------------------
     # region GET
+    def get_cluster_cut(self):
+        return where(self.get_data('Clusters', 'NClusters') == 1)
+
     def get_start_time(self):
-        return self.Run.StartTime
+        return datetime.fromtimestamp(self.Run.StartTime)
 
     def get_end_time(self):
-        return self.Run.EndTime
+        return datetime.fromtimestamp(self.Run.EndTime)
+
+    def get_time(self, cut=None):
+        t = array(self.Data['Time']).astype('f8') + self.Run.StartTime
+        return t if cut is None else t[cut]
+
+    def get(self, plane=None):
+        return self.Data['Plane{}'.format(self.get_plane(plane).Number)]
+
+    def get_group(self, grp, plane=None):
+        return self.Data['Plane{}'.format(self.get_plane(plane).Number)][grp]
 
     def get_data(self, grp, key=None, plane=None, cut=None):
-        data = self.Data['Plane{}'.format(self.get_plane(plane).Number)][grp]
+        data = self.get_group(grp, plane)
         data = array(data) if key is None else array(data[key])
         return data if cut is None else data[cut]
 
-    def get_x(self, plane=None, cluster=False, cut=None):
+    def get_n(self, name='Hits', cut=None):
+        return self.get_data(name, 'N{}'.format(name), cut=cut)
+
+    def get_split(self, cluster=True):
+        n = 'Clusters' if cluster else 'Hits'
+        return cumsum(self.get_n(n))[:-1]
+
+    def get_charges(self, cluster=True, e=False, cut=None, flat=False):
+        values = self.get_data('Clusters' if cluster else 'Hits', 'Charge', self.Plane, cut if flat else None) * (self.DUT.VcalToEl if e else 1)
+        if flat:
+            return values
+        return concatenate(array(split(values, self.get_split(cluster)))[cut])
+
+    def get_charge(self, cut=None):
+        values = self.get_charges(cut=cut, flat=True)
+        m, s = mean_sigma(values)
+        return ufloat(m, s / sqrt(values.size))
+
+    def get_x(self, plane=None, cluster=True, cut=None):
         return self.get_data('Clusters' if cluster else 'Hits', 'X', plane, cut)
 
-    def get_y(self, plane=None, cluster=False, cut=None):
+    def get_y(self, plane=None, cluster=True, cut=None):
         return self.get_data('Clusters' if cluster else 'Hits', 'Y', plane, cut)
 
     def get_hits(self, plane=None, cut=None):
@@ -97,7 +128,7 @@ class DUTAnalysis(Analysis):
     def get_clusters(self, plane=None, cut=None):
         return array([self.get_x(plane, cluster=True, cut=cut), self.get_y(plane, cluster=True, cut=cut)])
 
-    def get_mask(self, plane):
+    def get_mask(self, plane=None):
         return self.get_data('Mask', plane=plane)
 
     def get_mask_cut(self, plane=None):
@@ -127,10 +158,10 @@ class DUTAnalysis(Analysis):
         self.format_statbox(entries=True)
         self.draw_histo(h, show=show, lm=.12, draw_opt='box')
 
-    def draw_occupancy(self, plane=None, cluster=True, show=True):
+    def draw_occupancy(self, plane=None, cluster=True, cut=None, show=True):
         plane = self.get_plane(plane)
         h = TH2F('hto', '{} Occupancy in {}'.format('Cluster' if cluster else 'Hit', plane), *bins.get_local(plane))
-        cut = self.get_mask_cut(plane) if not cluster else None
+        cut = unique(concatenate([self.get_mask_cut(plane), [] if cut is None else cut])) if not cluster else cut
         fill_hist(h, *(self.get_clusters(plane, cut) if cluster else self.get_hits(plane, cut)))
         format_histo(h, x_tit='Column', y_tit='Row', y_off=1.5, z_tit='Number of Entries', z_off=1.2)
         self.format_statbox(entries=True, x=.83, m=True)
@@ -144,100 +175,80 @@ class DUTAnalysis(Analysis):
 
     def draw_cluster_size(self, plane=None, show=True):
         self.format_statbox(all_stat=True)
-        v = self.get_data('Clusters', 'NClusters', plane)
+        v = self.get_data('Clusters', 'Size', plane)
         self.draw_disto(v[v > 0], 'Cluster Size in {}'.format(self.get_plane(plane)), bins.make(0, 10), show=show, x_tit='Cluster Size', lm=.14, y_off=2)
 
     def draw_n_intercepts(self, plane=None, show=True):
         self.draw_n(plane, 'Intercepts', show)
 
-    def draw_charge_map(self, res=1, cut=''):
-        h = TProfile2D('pam', 'Charge Map', int(52 * res), .5, 52.5, int(80 * res), .5, 80.5)
-        self.Tree.Draw('ClusterVcal:ClusterY:ClusterX>>pam', TCut(cut), 'goff')
-        self.format_statbox(entries=1, x=.78)
-        format_histo(h, x_tit='Column', y_tit='Row', z_tit='Charge [vcal]', y_off=1.2, z_off=1.5)
-        self.draw_histo(h, draw_opt='colz', rm=.18)
+    def draw_charge_map(self, res=1, plane=None, cut=None):
+        plane = self.get_plane(plane)
+        p = TProfile2D('pam', 'Charge Map', *bins.get_local(plane, res))
+        fill_hist(p, self.get_x(plane, cut=cut), self.get_y(plane, cut=cut), self.get_charges(cut=cut, flat=True))
+        self.format_statbox(entries=True, x=.78)
+        format_histo(p, x_tit='Cluster X', y_tit='Cluster Y', z_tit='Charge [vcal]', y_off=1.2, z_off=1.5)
+        self.draw_histo(p, draw_opt='colz', rm=.18)
 
-    def draw_charge_distribution(self, vcal=True, cut='', x_range=None):
-        binning = [3756 / 4, -256, 3500] if vcal else [255, 0, 255]
-        title = 'VCAL' if vcal else 'ADC'
-        h = TH1I('had', '{} Distribution'.format(title), *binning)
-        self.Tree.Draw('{}>>had'.format('ClusterVcal' if vcal else 'Value'), TCut(cut), 'goff')
-        self.format_statbox(entries=1)
-        x_range = [h.GetBinCenter(i) for i in [h.FindFirstBinAbove(0) - 1, h.FindLastBinAbove(0) + 1]] if x_range is None else x_range
-        format_histo(h, x_tit=title, y_tit='Number of Entries', y_off=1.2, x_range=x_range)
-        self.draw_histo(h)
-
-    def draw_signal_distribution(self, x_range=None, cut=None, threshold=80 * 46.5, show=True):
-        h = TH1F('hph', 'Pulse Height Distribution', 50000 / 200, -5000, 45000)
-        self.Tree.Draw('ClusterVcal * 46.5 >> hph', self.Cut() if cut is None else cut, 'goff')
+    def draw_charge_distribution(self, bin_width=4, cut=None, x_range=None, show=True):
         self.format_statbox(all_stat=True)
-        format_histo(h, x_tit='Pulse Height [e]', y_tit='Number of Entries', y_off=1.6, x_range=x_range, ndivx=505, fill_color=self.FillColor)
-        self.draw_histo(h, lm=.12, show=show)
-        if threshold and show:
-            self.draw_y_axis(threshold, h.GetYaxis().GetXmin(), h.GetMaximum(), 'threshold #approx {}e  '.format(int(round_down_to(threshold, 100))), off=.3, line=True, opt='-L')
-        return h
+        self.draw_disto(self.get_charges(cut=cut, flat=True), 'Cluster Charge', bins.get_vcal(bin_width), x_tit='Charge [vcal]', x_range=x_range, show=show)
+
+    def draw_signal_distribution(self, bin_width=200, x_range=None, cut=None, show=True):
+        self.format_statbox(all_stat=True)
+        values = self.get_charges(cut=cut, flat=True) * self.DUT.VcalToEl
+        return self.draw_disto(values, 'Pulse Height', bins.get_electrons(bin_width), x_tit='Pulse Height [e]', x_range=x_range, show=show)
 
     def draw_trigger_phase(self):
-        pass
+        self.format_statbox(all_stat=True)
+        self.draw_disto(self.get_data('TriggerPhase'), 'Trigger Phase', bins.make(0, 10), x_tit='Trigger Phase', y_off=1.8, lm=.13)
 
     def draw_charge_vs_trigger_phase(self):
         pass
 
-    def draw_charge_vs_time(self, bin_width=30, y_range=None, show=True):
-        prof = TProfile('pct', 'Charge vs. Time - Run {}'.format(self.RunNumber), int((self.EndTime - self.StartTime) / bin_width), self.StartTime, self.EndTime)
-        self.Tree.Draw('ClusterVcal:TimeStamp / 1000>>pct', '', 'goff')
-        y_range = [0, 1.2 * prof.GetMaximum()] if y_range is None else y_range
-        format_histo(prof, x_tit='Time [hh:mm]', y_tit='Charge [vcal]', y_off=1.4, stats=0, fill_color=self.FillColor, t_ax_off=0, y_range=y_range)
-        self.draw_histo(prof, lm=.11, show=show)
-        return prof
+    def draw_charge_trend(self, bin_width=30, e=False, y_range=None, show=True, stats=True):
+        self.format_statbox(entries=True, exe=stats)
+        cut = self.get_cluster_cut()
+        t = self.get_time(cut)
+        charges = self.get_charges(e=e, cut=cut)
+        p = self.draw_prof(t, charges, bins.make(t[0], t[-1], bin_width), x_tit='Time [hh:mm}', y_tit='Charge [{}]'.format('e' if e else 'vcal'), show=show, t_ax_off=0)
+        values = get_hist_vec(p)
+        format_histo(p, y_range=choose(y_range, increased_range([min(values).n, max(values).n], 1, 2)))
+        update_canvas()
+        return p
 
-    def fit_charge(self, bin_width=30, y_range=None):
-        self.format_statbox(only_fit=True, y=.5)
-        h = self.draw_charge_vs_time(bin_width=bin_width, y_range=y_range)
+    def fit_charge(self, bin_width=30, y_range=None, show=False):
+        self.format_statbox(only_fit=True)
+        h = self.draw_charge_trend(bin_width=bin_width, y_range=y_range, show=show, stats=False)
         format_histo(h, stats=1, name='Fit Result')
         fit = h.Fit('pol0', 'sq')
         return fit
 
-    def draw_efficiency(self, bin_width=30, cut='', show=True):
-        prof = TProfile('pet', 'Trigger Efficiency - Run {}'.format(self.RunNumber), int((self.EndTime - self.StartTime) / bin_width), self.StartTime, self.EndTime)
-        self.Tree.Draw('(NHits>0)*100.:TimeStamp / 1000>>pet', TCut(cut), 'goff')
-        format_histo(prof, x_tit='Time [hh:mm]', y_tit='Efficiency [%]', y_off=1.4, stats=0, fill_color=self.FillColor, t_ax_off=0, y_range=[0, 105])
-        self.draw_histo(prof, lm=.11, show=show)
-        return prof
+    def draw_efficiency(self, bin_width=30, show=True):
+        cut = self.Tracks.get_n() > 0
+        eff = (self.get_n(cut=cut) > 0).astype('u2') * 100
+        t = self.get_time(cut)
+        return self.draw_prof(t, eff, bins.make(t[0], t[-1], bin_width), x_tit='Time [hh:mm]', y_tit='Efficiency [%]', t_ax_off=0, y_range=[0, 105], show=show, stats=0)
 
-    def fit_efficiency(self, bin_width=30, cut=''):
-        self.format_statbox(only_fit=True, y=.3)
-        h = self.draw_efficiency(bin_width=bin_width, cut=cut)
+    def fit_efficiency(self, bin_width=30):
+        self.format_statbox(only_fit=True)
+        h = self.draw_efficiency(bin_width=bin_width)
         format_histo(h, stats=1, name='Fit Result')
         fit = h.Fit('pol0', 'sq')
         return fit
 
     def draw_efficiency_vs_trigger_phase(self):
-        prof = TProfile('petp', 'Trigger Efficiency @ Trigger Phase', 10, 0, 10)
-        self.Tree.Draw('(NHits>0)*100.:Timing>>petp', '', 'goff')
-        format_histo(prof, x_tit='Trigger Phase', y_tit='Trigger Efficiency [%]', y_off=1.2, stats=0, y_range=[0, 105])
-        self.draw_histo(prof)
+        # add profile
+        pass
+
+    def draw_time(self, show=True):
+        t = self.get_time()
+        g = self.make_graph_from_profile(self.draw_prof(arange(t.size), t, bins.make(0, t.size, sqrt(t.size)), show=False), err=0)
+        format_histo(g, x_tit='Event Number', y_tit='Time [hh:mm]', y_off=1.8)
+        set_time_axis(g, axis='Y')
+        self.draw_histo(g, show, .13, draw_opt='al')
+
     # endregion DRAW
     # ----------------------------------------
-
-    def draw_calibration_fit(self, col=31, row=55):
-        f = open(join(self.TCDir, self.Run.DUTName, 'calibrations', 'phCal{}.dat'.format(self.get_calibration_number())))
-        f.readline()
-        low_range = [int(val) for val in f.readline().split(':')[-1].split()]
-        high_range = [int(val) for val in f.readline().split(':')[-1].split()]
-        x = [ufloat(xval, 1) for xval in low_range + [val * 7 for val in high_range]]
-        f.readline()
-        self.Fit.SetParameters(*self.FitParameters[col][row])
-        for line in f.readlines():
-            data = line.split('Pix')
-            icol, irow = [int(val) for val in data[-1].split()]
-            if col == icol and row == irow:
-                y = [ufloat(int(val), 1) for val in data[0].split()]
-                g = self.make_tgrapherrors('gcal', 'Calibration Fit for Pix {} {}'.format(col, row), x=x, y=y)
-                format_histo(g, x_tit='vcal', y_tit='adc', y_off=1.4)
-                self.draw_histo(g)
-                self.Fit.Draw('same')
-                break
 
     def fit_langau(self, h=None, nconv=30, show=True, chi_thresh=8, fit_range=None):
         h = self.draw_signal_distribution(show=show) if h is None and hasattr(self, 'draw_signal_distribution') else h
@@ -269,14 +280,15 @@ class DUTAnalysis(Analysis):
 
 
 if __name__ == '__main__':
-    t = time()
-    p = ArgumentParser()
-    p.add_argument('run', nargs='?', default=11, type=int)
-    p.add_argument('dut', nargs='?', default=1, type=int)
-    p.add_argument('--testcampaign', '-tc', nargs='?', default=None)
-    p.add_argument('--verbose', '-v', action='store_true')
-    p.add_argument('--single_mode', '-s', action='store_false')
-    args = p.parse_args()
+    t_start = time()
+    aparser = ArgumentParser()
+    aparser.add_argument('run', nargs='?', default=11, type=int)
+    aparser.add_argument('dut', nargs='?', default=1, type=int)
+    aparser.add_argument('--testcampaign', '-tc', nargs='?', default=None)
+    aparser.add_argument('--verbose', '-v', action='store_true')
+    aparser.add_argument('--single_mode', '-s', action='store_false')
+    args = aparser.parse_args()
     z = DUTAnalysis(args.run, args.dut, test_campaign=args.testcampaign, single_mode=args.single_mode, verbose=args.verbose)
-    c = z.Calibration
-    z.add_info(t, prnt=True)
+    cal = z.Calibration
+    c = z.Converter
+    z.add_info(t_start, prnt=True)
