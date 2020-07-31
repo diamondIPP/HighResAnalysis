acbf9fd18592f4e78504379a2af14e151fe3dbf3
michareichmann <micha.reichmann@gmail.com>
2020-07-31 14:11:12 +0200

    merge all telescope methods to telescope module

--- src/telescope.py
+++ src/telescope.py
@@ -19,21 +19,49 @@ class TelescopeAnalysis(Analysis):
         self.Plane = Plane(0, self.Ana.Config)
         self.NPlanes = self.Config.getint('TELESCOPE', 'planes')
 
-    def draw_occupancy(self, plane=0, cluster=True, bin_width=10, show=True):
-        return self.Ana.draw_occupancy(self.Plane(plane), cluster, bin_width, show=show)
+    # ----------------------------------------
+    # region GET
+    def get(self, plane=0):
+        return self.Data['Plane{}'.format(self.Plane(plane).Number)]
+
+    def get_group(self, grp, plane=0):
+        return self.get(plane)[grp]
+
+    def get_data(self, grp, key=None, plane=0, cut=None):
+        data = self.get_group(grp, plane)
+        data = array(data) if key is None else array(data[key])
+        return data[self.Cuts(cut)]
+
+    def get_n(self, name='Hits', plane=0, cut=None):
+        return self.get_data(name, 'N{}'.format(name), plane, cut)
+
+    def get_x(self, plane=0, cluster=True, cut=None):
+        return self.get_data('Clusters' if cluster else 'Hits', 'X', plane, cut)
+
+    def get_y(self, plane=0, cluster=True, cut=None):
+        return self.get_data('Clusters' if cluster else 'Hits', 'Y', plane, cut)
+
+    def get_coods(self, plane=0, cluster=True, cut=None):
+        return self.get_x(plane, cluster, cut), self.get_y(plane, cluster, cut)
+    # endregion GET
+    # ----------------------------------------
+
+    def draw_n(self, plane=0, name='Hits', show=True):
+        self.format_statbox(all_stat=True)
+        n, pl = name, plane
+        self.draw_disto(self.get_n(n, pl), 'Number of {} in {}'.format(n, pl), bins.make(0, 30), lm=.13, show=show, x_tit='Number of {}'.format(n), y_off=2)
+
+    def draw_occupancy(self, plane=0, cluster=True, bin_width=10, cut=None, show=True):
+        self.format_statbox(entries=True, x=.83, m=True)
+        x, y = self.get_coods(plane, cluster, cut)
+        title = '{} Occupancy of Plane {}'.format('Cluster' if cluster else 'Hit', plane)
+        self.draw_histo_2d(x, y, title, bins.get_local(self.Plane, bin_width, aspect_ratio=True), x_tit='Column', y_tit='Row', show=show)
 
     def draw_n_hits(self, plane=0, show=True):
-        self.Ana.draw_n(self.Plane(plane), 'Hits', show)
+        self.draw_n(plane, 'Hits', show)
 
     def draw_n_clusters(self, plane=0, show=True):
-        self.Ana.draw_n(self.Plane(plane), 'Clusters', show)
+        self.draw_n(plane, 'Clusters', show)
 
     def draw_n_intercepts(self, plane=0, show=True):
-        self.Ana.draw_n(self.Plane(plane), 'Intercepts', show)
-
-    def draw_cluster_size(self, plane=0, show=True):
-        self.Ana.draw_cluster_size(self.Plane(plane), show)
-
-    def get_1_cluster_cut(self, plane=0):
-        n = self.Ana.get_n_clusters(self.Plane(plane))
-        return n == 1
+        self.draw_n(plane, 'Intercepts', show)
