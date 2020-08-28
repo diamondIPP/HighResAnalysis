11e74fcbf1e50190d4601fc03db55bebd82018fc
michareichmann <micha.reichmann@gmail.com>
2020-08-28 08:08:50 +0200

    add rotation and translations for the track intercepts

--- src/tracks.py
+++ src/tracks.py
@@ -4,10 +4,11 @@
 # created on June 10th 2020 by M. Reichmann (remichae@phys.ethz.ch)
 # --------------------------------------------------------
 
-from analysis import Analysis, warning
+from analysis import Analysis, rotate, do_pickle, get_rot_matrix
 from draw import fill_hist, format_histo, array
 from ROOT import TH2F
-from numpy import rad2deg
+from numpy import rad2deg, ndarray, dot
+from numpy.linalg import inv
 import bins
 
 
@@ -23,11 +24,9 @@ class TrackAnalysis(Analysis):
 
     # ----------------------------------------
     # region GET
-    def get(self, key, cut=None):
-        if key not in self.Data.keys():
-            warning('key {} not found in {}'.format(key, list(self.Data.keys())))
-            return
-        return array(self.Data[key])[self.Cuts(cut)]
+    def get(self, key, cut=None, trk_cut=None):
+        data = array(self.Data[key])
+        return data[self.Cuts(trk_cut)] if type(trk_cut) == ndarray or trk_cut != -1 else data[self.Ana.Cuts.get('cluster').Values][self.Cuts(cut)]
 
     def get_n(self, cut=None):
         """ returns: number of tracks per event. """
@@ -39,14 +38,19 @@ class TrackAnalysis(Analysis):
     def get_y(self, cut=None, trk_cut=-1, local=True):
         return self.Ana.get_track_data('Tracks', 'Y' if local else 'V', cut, trk_cut)
 
-    def get_coods(self, local=True, cut=None, trk_cut=-1):
-        return (self.get_x(cut, trk_cut), self.get_y(cut, trk_cut)) if local else (self.get_u(cut, trk_cut), self.get_v(cut, trk_cut))
+    def get_trans_matrix(self, local=True):
+        rot = get_rot_matrix(self.get_rot_angle())
+        return dot(inv(self.Ana.Plane.M), dot(rot, self.Ana.Plane.M)) if local else rot
 
-    def get_dof(self, cut=None):
-        return self.get('Dof', cut)
+    def get_coods(self, local=True, cut=None, trk_cut=-1, trans=True, off=True):
+        x, y = (self.get_x(cut, trk_cut), self.get_y(cut, trk_cut)) if local else (self.get_u(cut, trk_cut), self.get_v(cut, trk_cut))
+        return dot(self.get_trans_matrix(local), array([x, y])) + (self.get_offsets() if off else 0) if trans else array([x, y])
 
-    def get_chi2(self, cut=None):
-        return self.get('Chi2', cut) / self.get_dof(cut)
+    def get_dof(self, cut=None, trk_cut=None):
+        return self.get('Dof', cut, trk_cut)
+
+    def get_chi2(self, cut=None, trk_cut=None):
+        return self.get('Chi2', cut, trk_cut) / self.get_dof(cut, trk_cut)
 
     def get_u(self, cut=None, trk_cut=-1):
         return self.Ana.get_track_data('Tracks', 'U', cut, trk_cut)
@@ -54,8 +58,26 @@ class TrackAnalysis(Analysis):
     def get_v(self, cut=None, trk_cut=-1):
         return self.Ana.get_track_data('Tracks', 'V', cut, trk_cut)
 
-    def get_events(self, cut=False):
-        return self.get('EvtFrame', cut=cut)
+    def get_events(self, cut=False, trk_cut=False):
+        return self.get('EvtFrame', cut=cut, trk_cut=trk_cut)
+
+    def get_rot_angle(self, redo=False):
+        def f():
+            angles = [self.Ana.draw_udv(show=False)]
+            x, y = self.get_coods(local=False, trans=False)
+            for _ in range(4):
+                x, y = rotate(angles[-1], array([x, y]))
+                angles.append(self.Ana.draw_udv(tv=y, show=False))
+            return sum(angles)
+        return do_pickle(self.make_pickle_path('rot'), f, redo=redo)
+
+    def get_offsets(self, redo=False):
+        def f():
+            x, y = self.get_coods(trans=True, off=False)
+            dx, dy = self.Ana.get_x() - x, self.Ana.get_y() - y
+            xfit, yfit = [self.draw_disto(v, show=False).Fit('gaus', 'qs0') for v in [dx, dy]]
+            return array([[xfit.Parameter(1)], [yfit.Parameter(1)]])
+        return do_pickle(self.make_pickle_path('off'), f, redo=redo)
     # endregion GET
     # ----------------------------------------
 
@@ -86,10 +108,10 @@ class TrackAnalysis(Analysis):
         binning = bins.get_global(self.Ana.Plane if dut_plane else self.Ana.Telescope.Plane, bin_width)
         self.draw_histo_2d(self.get_u(cut), self.get_v(cut), binning, 'Track Map', x_tit='Track X [mm]', y_tit='Track Y [mm]', show=show)
 
-    def draw_chi2(self, cut=None):
+    def draw_chi2(self, cut=None, trk_cut=None):
         self.format_statbox(all_stat=True)
-        values = self.get_chi2(cut)
-        self.draw_disto(values, bins.make(0, 100, 1.), 'Track #chi^{2}', x_tit='#chi^{2}')
+        values = self.get_chi2(cut, trk_cut)
+        return self.draw_disto(values, bins.make(0, 100, 1.), 'Track #chi^{2}', x_tit='#chi^{2}')
 
     def draw_slope(self, mode='x', bin_width=.01):
         values = rad2deg(array(self.Data['Slope{}'.format(mode.title())]))
