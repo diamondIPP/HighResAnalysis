013432e1dc6b4f5c8920973481621d272a9d673d
michareichmann <micha.reichmann@gmail.com>
2021-10-28 18:08:25 +0200

    add show_structure()

--- src/dut_analysis.py
+++ src/dut_analysis.py
@@ -23,6 +23,7 @@ from src.tracks import TrackAnalysis
 import src.bins as bins
 
 
+# todo add ADC to data!
 class DUTAnalysis(Analysis):
 
     def __init__(self, run_number, dut_number, test_campaign, single_mode=True, verbose=True, test=False):
@@ -74,6 +75,7 @@ class DUTAnalysis(Analysis):
             self.Converter.run()
             try:
                 f = self.load_file()
+                # check if data is complete
                 _ = f['Tracks']
                 _ = f[self.Plane.get_name()]
                 return f
@@ -86,6 +88,17 @@ class DUTAnalysis(Analysis):
 
     def reload_data(self):
         self.Data = self.load_data()
+
+    def show_structure(self):
+        print('.')
+        for i0, (key, grp) in enumerate(self.Data.items()):
+            if 'Plane' not in key or key == 'Plane0' or key == f'Plane{self.Plane.Number}':
+                print(f'├── {key}')
+                for k, g in grp.items():
+                    print(f'{"│" if i0 < len(self.Data.keys()) - 1 else " "}   ├── {k}')
+                    if hasattr(g, 'keys'):
+                        for i in g.keys():
+                            print(f'│   │   ├── {i}')
     # endregion INIT
     # ----------------------------------------
 
@@ -409,13 +422,13 @@ class DUTAnalysis(Analysis):
     def draw_y_correlation(self, res=1, plane=2, show=True):
         self.draw_correlation('y', res, plane, show)
 
-    def draw_correlation_trend(self, plane=2, bin_width=120, thresh=.5, show=True):
+    def draw_correlation_trend(self, plane=2, bin_width=120, thresh=.5, **dkw):
         v0, v1 = self.get_y(cut=self.make_correlation(plane)), self.Telescope.get_y(plane, cut=self.Telescope.make_correlation(plane))
         splits, t = histogram(self.get_time(self.make_correlation(plane)), bins.get_time(self.get_time(), min(int(self.Duration / 3), bin_width))[1])
         hs = [histogram2d(v[0], v[1], [bins.get_local_y(self.Plane, 2)[1], bins.get_local_y(self.Telescope.Plane, 4)[1]])[0] for v in split(array([v0, v1]), cumsum(splits)[:-1], axis=1)]
         # take only bins with >25% of max entries (noise filter)
         values = [corrcoef(array(where(h > max(h) * thresh)).repeat(h[h > max(h) * thresh].astype('i'), axis=1).T, rowvar=False)[0][1] for h in hs]
-        return self.Draw.graph(t[:-1] + diff(t) / 2, y=values, y_tit='Correlation Factor', **self.get_time_args(), y_range=[0, 1.05], draw_opt='ap')
+        return self.Draw.graph(t[:-1] + diff(t) / 2, y=values, **prep_kw(dkw, y_tit='Correlation Factor', **self.get_time_args(), y_range=[0, 1.05], draw_opt='ap'))
 
     def draw_alignment(self, plane=2, bin_width=120, thresh=.5, show=True):
         x, c = get_graph_vecs(self.draw_correlation_trend(plane, bin_width, show=False), err=False)
@@ -464,9 +477,9 @@ class DUTAnalysis(Analysis):
         x, y = self.get_trigger_phase(cut=cut), self.get_charges(cut=cut)
         self.Draw.profile(x, y, bins.get_triggerphase(), 'Charge vs. Trigger Phase', show=show, x_tit='Trigger Phase', y_tit='Charge [vcal]')
 
-    def draw_charge_trend(self, bin_width=30, e=False, y_range=None, cut=None, show=True, stats=True):
+    def draw_charge_trend(self, bin_width=30, e=False, y_range=None, cut=None, **dkw):
         t, charges = self.get_time(cut), self.get_charges(e=e, cut=cut)
-        p = self.Draw.profile(t, charges, bins.get_time(t, bin_width), x_tit='Time [hh:mm}', y_tit='Charge [{}]'.format('e' if e else 'vcal'), show=show, t_ax_off=0)
+        p = self.Draw.profile(t, charges, bins.get_time(t, bin_width), **prep_kw(dkw, **self.get_time_args(), y_tit='Charge [{}]'.format('e' if e else 'vcal')))
         values = get_hist_vec(p)
         # noinspection PyUnresolvedReferences
         format_histo(p, y_range=choose(y_range, increased_range([min(values).n, max(values).n], 1, 2)))
@@ -526,9 +539,9 @@ class DUTAnalysis(Analysis):
                 self.Draw.tlatex(ix + nx / 2 - .5, iy + ny / 2 - .5, '{:2.1f}'.format(e[i][j]), str(i * y.size + j), 22, size=.02)
         self.draw_segments(nx, ny)
 
-    def draw_segment_distribution(self, nx=2, ny=3, full=False, cut=None, bin_width=None, segments=True, show=True):
+    def draw_segment_distribution(self, nx=2, ny=3, zsup=False, cut=None, bin_width=None, segments=True, show=True):
         e = self.get_segment_efficiecies(nx, ny, cut).flatten() if segments else get_2d_hist_vec(self.draw_efficiency_map(.5, show=False), err=False)
-        binning = bins.make(0, 101.5, choose(bin_width, 1)) if full else bins.make(95, 100.5, choose(bin_width, 5.5 / sqrt(e.size) / 2), last=True)
+        binning = bins.make(0, 101.5, choose(bin_width, 1)) if zsup else bins.make(95, 100.5, choose(bin_width, 5.5 / sqrt(e.size) / 2), last=True)
         self.Draw.distribution(e, binning, 'Segment Efficiencies', x_tit='Efficiency [%]', show=show)
 
     def draw_inpixel_eff(self, res=.1, cut=None, show=True, cell=False):
