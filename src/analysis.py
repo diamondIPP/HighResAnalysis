561d1f0c779af350029eafd32817f71169904bba
michareichmann <micha.reichmann@gmail.com>
2022-03-17 23:19:19 +0100

    complete revision and add BeamTest class

--- src/analysis.py
+++ src/analysis.py
@@ -1,74 +1,79 @@
-from glob import glob
-from os.path import basename, expanduser
+from os.path import expanduser
+from os import getcwd
 from shutil import copyfile
 
 from plotting.draw import *
-from src.utils import PBar, BaseDir, print_banner
+from src.utils import PBar, Dir, print_banner
 from src.cut import Cuts
 
-# global test campaign
-g_test_campaign = None
+
+class BeamTest:
+
+    def __init__(self, p: Path):
+
+        self.Path = p
+        self.Location = p.parts[-2].upper()
+        self.T = datetime.strptime(p.stem, '%Y-%m')
+        self.Year = self.T.year
+
+    def __str__(self):
+        return self.T.strftime('%b %Y')
+
+    def __repr__(self):
+        return f'Beam Test in {self} at {self.Location}'
+
+
+def load_config():
+    config_file_path = join(Dir, 'config', 'main.ini')
+    if not isfile(config_file_path):
+        warning('The main config file "config/main.ini" does not exist! Using the default!')
+        copyfile(join(Dir, 'config', 'default.ini'), config_file_path)
+    return Config(config_file_path)
 
 
 class Analysis:
     """ The analysis class provides default behaviour objects in the analysis framework and is the parent of all other analysis objects. """
 
-    def __init__(self, testcampaign=None, verbose=False):
+    Config = load_config()
+    Locations = Config.get_list('MAIN', 'locations')
+
+    DataDir = expanduser(Config.get('MAIN', 'data directory'))
+    ResultsDir = join(Dir, 'results')
+    MetaDir = join(Dir, Config.get('SAVE', 'meta directory'))
+
+    def __init__(self, beamtest=None, verbose=False):
 
         self.Verbose = verbose
-        self.Config = self.load_config()
-        self.Locations = loads(self.Config.get('MAIN', 'locations'))
 
-        # test campaign
-        self.TestCampaign = self.load_test_campaign(testcampaign)
-        self.Location = self.get_test_campaigns()[self.TestCampaign]
-        self.TCString = self.generate_tc_str()
-
-        # Directories
-        self.DataDir = join(self.get_raw_data_dir(), str(self.Location.lower()))
-        self.TCDir = self.generate_tc_directory()
-        self.MetaDir = join(BaseDir, self.Config.get('SAVE', 'meta directory'))
+        self.BeamTest = self.load_test_campaign(beamtest)
         self.MetaSubDir = ''
-        self.ResultsDir = join(BaseDir, 'results')
 
         self.Cuts = Cuts()
 
-        # progress bar
         self.PBar = PBar()
-        self.Draw = Draw(self.Config.FileName, self.Verbose)
+        self.Draw = Draw(Analysis.Config.FilePath, self.Verbose)
+
+    def __str__(self):
+        return f'{self.__class__.__name__.replace("Analysis", "").upper()} ANALYSIS'
+
+    def __repr__(self):
+        return f'{self} OF {self.BeamTest!r}'
 
     # ----------------------------------------
     # region INIT
     @staticmethod
-    def load_config():
-        config_file_path = join(BaseDir, 'config', 'main.ini')
-        if not isfile(config_file_path):
-            warning('The main config file "config/main.ini" does not exist! Using the default!')
-            copyfile(join(BaseDir, 'config', 'main.default'), config_file_path)
-        return Config(config_file_path)
+    def load_test_campaign(beamtest=None):
+        bt = choose(beamtest, Analysis.Config.get('MAIN', 'default test campaign'))
+        ps = [p for loc in Analysis.Locations for p in Path(Analysis.DataDir, loc.lower()).glob('*')]
+        p = next((p for p in ps if bt == p.stem.replace('-', '')), None)
+        return critical(f'The beamtest "{bt}" does not exist!') if p is None else BeamTest(p)
 
-    def get_raw_data_dir(self):
-        return expanduser(self.Config.get('MAIN', 'data directory'))
-
-    def generate_tc_str(self):
-        return self.TestCampaign.strftime('%b %Y')
-
-    def generate_tc_directory(self):
-        return join(self.DataDir, self.TestCampaign.strftime('%Y-%m'))
-
-    def load_test_campaign(self, testcampaign):
-        global g_test_campaign
-        if g_test_campaign is None:
-            g_test_campaign = datetime.strptime(self.Config.get('MAIN', 'default test campaign') if testcampaign is None else testcampaign, '%Y%m')
-        if g_test_campaign not in self.get_test_campaigns():
-            critical('The Testcampaign {} does not exist!'.format(g_test_campaign))
-        return g_test_campaign
-
-    def get_test_campaigns(self):
-        return {datetime.strptime(basename(path), '%Y-%m'): loc for loc in self.Locations for path in glob(join(self.get_raw_data_dir(), loc.lower(),  '*'))}
+    @staticmethod
+    def find_testcampaign(default=None):
+        return choose(default, BeamTest(Path(getcwd())))
 
     def print_testcampaign(self):
-        self.info('TESTCAMPAIGN: {}'.format(self.generate_tc_str()))
+        self.info(f'{self.BeamTest!r}')
     # endregion INIT
     # ----------------------------------------
 
@@ -78,20 +83,16 @@ class Analysis:
     def add_info(self, t, msg='Done', prnt=None):
         add_to_info(t, msg, choose(prnt, self.Verbose))
 
-    def set_pickle_sub_dir(self, name):
+    def set_meta_sub_dir(self, name):
         self.MetaSubDir = name
 
     def make_pickle_path(self, name='', suf='', sub_dir=None, run=None, dut=None, camp=None):
         directory = join(self.MetaDir, self.MetaSubDir if sub_dir is None else sub_dir)
         ensure_dir(directory)
-        campaign = choose(camp, self.TestCampaign.strftime('%Y%m'))
-        # noinspection PyUnresolvedReferences
+        campaign = choose(camp, self.BeamTest.T.strftime('%Y%m'))
         dut = str(dut if dut is not None else self.DUT.Number if hasattr(self, 'DUT') and hasattr(self.DUT, 'Number') else '')
-        return join(directory, '{}.pickle'.format('_'.join([v for v in [name, campaign, self.make_run_str(run), dut, str(suf)] if v])))
-
-    def make_run_str(self, run_str=None):
-        run = self.RunPlan if hasattr(self, 'RunPlan') else self.Run.Number if hasattr(self, 'Run') and hasattr(self.Run, 'Number') else ''
-        return choose(run_str, str(run))
+        run = choose(run, self.RunPlan if hasattr(self, 'RunPlan') else str(self.Run) if hasattr(self, 'Run') else '')
+        return join(directory, f'{"_".join([v for v in [name, campaign, run, dut, str(suf)] if v])}.pickle')
 
     def make_hdf5_path(self, *args, **kwargs):
         return self.make_pickle_path(*args, **kwargs).replace('pickle', 'hdf5')
@@ -100,7 +101,7 @@ class Analysis:
         if prnt:
             ana_name = self.__class__.__name__.replace('Analysis', '')
             run = ' FOR RUN{} {}'.format('PLAN' if 'Coll' in ana_name else '', run) if run is not None else ''
-            tc = ' OF {}'.format(self.TCString) if tc else ''
+            tc = f' OF {self.BeamTest}' if tc else ''
             print_banner('STARTING {} ANALYSIS{}{}'.format(ana_name.upper(), run, tc), symbol='~', color=GREEN)
 
 
