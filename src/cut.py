819acd769fb88249457862ea8919f656650b6ea7
michareichmann <micha.reichmann@gmail.com>
2022-04-03 08:01:05 +0200

    revise cut base class

--- src/cut.py
+++ src/cut.py
@@ -4,11 +4,11 @@
 # created on July 10th 2020 by M. Reichmann (remichae@phys.ethz.ch)
 # --------------------------------------------------------
 from typing import Any
-from numpy import array, all, in1d, invert, ones
+from numpy import array, all, in1d, invert, ones, count_nonzero, max
 
 from src.utils import print_table, warning, join, make_list, choose, Dir, is_iter
 from plotting.utils import Config
-from plotting.draw import make_box_args
+from plotting.draw import make_box_args, Draw, prep_kw, TCutG
 
 
 class Cuts:
@@ -16,10 +16,11 @@ class Cuts:
 
     def __init__(self):
 
-        self.Config = Config(join(Dir, 'cuts', 'cut.ini'))
+        self.Config = self.init_config()
         self.Cuts = {}
+        self.make()
 
-    def __call__(self, cut=None):
+    def __call__(self, cut=None, pl=None):
         cut = cut.Values if isinstance(cut, Cut) else cut
         values = array(cut)
         return values if values.size > 1 else self.generate() if cut is None else ...
@@ -30,16 +31,32 @@ class Cuts:
     def __getitem__(self, v):
         return self.get(v)()
 
-    def set_config(self, beamtest, dut):
-        self.Config = Config(join(Dir, 'cuts', f'cut{beamtest}.ini'), section=dut)
+    def __repr__(self):
+        k, n = (count_nonzero(self()), self().size) if self.n else (0, 0)
+        return f'{self.__class__.__name__} instance with {self.n} cuts' + (f', selecting {k}/{n} events ({100 * k / n:.1f} %)' if n else '')
+
+    def init_config(self):
+        return Config(join(Dir, 'cuts', 'cut.ini'))
+
+    def make(self, redo=False):
+        pass
 
     def get_config(self, option, dtype=str):
         v = self.Config.get_value(option, dtype=dtype)
         return array(v) if is_iter(v) else v
 
     def get_fid_config(self, surface=False):
-        p = self.get_config(f'{"surface " if surface else ""}fiducial')
-        return make_box_args(*p[[0, 2, 1, 3]] + array([0, 0, 1, 1])) if p.size == 4 else p
+        p = self.get_config('surface fiducial' if surface else 'fiducial')
+        p = make_box_args(*p[[0, 2, 1, 3]]) if p.size == 4 else p  # unpack short box notation
+        p[p == max(p, axis=1).reshape((-1, 1))] += 1  # extend one pixel to the top and right
+        return p - .5  # pixel centre is at the integer
+
+    def get_fid(self, surface=False, **dkw):
+        x, y = self.get_fid_config(surface)
+        return Draw.polygon(x, y, **prep_kw(dkw, show=False, line_color=2, width=2, name=f'fid{surface:d}'))
+
+    def draw_fid(self, surface=False, **dkw):
+        self.get_fid(surface, **prep_kw(dkw, show=True))
 
     def generate(self):
         cuts = [cut.Values for cut in self.Cuts.values() if cut.Level < 80]
@@ -51,13 +68,18 @@ class Cuts:
             cut.set_level(choose(level, cut.Level))
             self.Cuts[cut.Name] = cut
         else:
-            self.Cuts[name] = Cut(name, values, level, description)
+            if not all(values):
+                self.Cuts[name] = Cut(name, values, level, description)
 
     def get(self, name):
         return self.Cuts[name]
 
+    @property
+    def n(self):
+        return len([cut for cut in self.Cuts.values() if cut.Level < Cut.MaxLevel])
+
     def get_consecutive(self):
-        cuts = sorted([cut for cut in self.Cuts.values() if cut.Level < 80])
+        cuts = sorted([cut for cut in self.Cuts.values() if cut.Level < Cut.MaxLevel])
         consecutive = ones(cuts[0].Size, dtype=bool)
         p0 = 0
         for cut in cuts:
@@ -69,6 +91,9 @@ class Cuts:
 
     def get_nofid(self, cut=None, fid=False):
         return self(cut) if fid else self.exclude('fid', cut)
+
+    def include(self, *names):
+        return all([self[n] for n in names], axis=0)
 
     def exclude(self, exclude, cut=None):
         if cut is not None:
@@ -82,16 +107,21 @@ class Cuts:
         c = Cut('all', self.generate(), 0, 'final cut')
         print_table([row for row in rows if row[2]], ['Cut Name', 'Level', 'Size', 'P', 'Description'], [c.Name, '', c.Size, c.get_p_str(), c.Description])
 
-    @staticmethod
-    def make_mask(x, y, masked_pixels):
+    def make_pixel_mask(self, x, y):
         data = x.astype('i') * 10000 + y  # make unique number out of the tuple... Is there a way to compare tuples?
-        mx, my = masked_pixels.T.astype('i')
+        mx, my = array(self.get_config('mask')).T
         mask = mx * 10000 + my
         return in1d(data, mask, invert=True)
+
+    @staticmethod
+    def point_in_polygon(p, poly: TCutG):
+        return poly.IsInside(*p)
 
 
 class Cut:
     """ Base class to describe a single cut """
+
+    MaxLevel = 80
 
     def __init__(self, name, values: Any, level=99, description=None):
 
@@ -101,6 +131,7 @@ class Cut:
         self.Description = description
         self.Size = self.Values.size
         self.P = self.calc_p()
+        self.N = round(self.Size * (1 - self.P))
 
     def __call__(self):
         return self.Values
