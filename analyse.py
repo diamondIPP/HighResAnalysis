29c43ea4558bc26b2381644eb16b030bd10db27c
michareichmann <micha.reichmann@gmail.com>
2022-04-03 12:08:10 +0200

    update aliases

--- analyse.py
+++ analyse.py
@@ -4,19 +4,20 @@
 # created on March 18th 2022 by M. Reichmann (remichae@phys.ethz.ch)
 # --------------------------------------------------------
 
-from src.dut_analysis import DUTAnalysis, Analysis
-from src.utils import *  # noqa
+from argparse import ArgumentParser
+from numpy import *
+
 import src.bins as bins  # noqa
 from plotting.draw import *
-from argparse import ArgumentParser
-
+from src.dut_analysis import DUTAnalysis, Analysis
+from src.utils import *  # noqa
 
 t_start = time()
 aparser = ArgumentParser()
 aparser.add_argument('run', nargs='?', default=11, type=int)
 aparser.add_argument('dut', nargs='?', default=1, type=int)
 aparser.add_argument('--testcampaign', '-tc', nargs='?', default=Analysis.find_testcampaign())
-aparser.add_argument('--verbose', '-v', action='store_true')
+aparser.add_argument('--verbose', '-v', action='store_false')
 aparser.add_argument('--single_mode', '-s', action='store_false')
 aparser.add_argument('--test', '-t', action='store_true')
 args = aparser.parse_args()
@@ -26,14 +27,15 @@ z.add_info(t_start, prnt=True)
 
 # aliases
 try:
-    c = z.Converter
-    cal = z.Calibration
-    cut = z.Cuts
-    cur = z.Currents
-    r = z.Run
     dut = z.DUT
-    t = z.Tracks
+    r = z.Run
+    c = z.Converter
+    cut = z.Cut
+    cal = z.Calibration
     tel = z.Tel
+    ref = z.REF
+    t = z.Tracks
+    cur = z.Currents
     e = z.Efficiency
 except AttributeError:
     pass
