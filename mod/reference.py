818304226ada77e60c882a2a54aa24f90578766e
michareichmann <micha.reichmann@gmail.com>
2022-04-05 16:39:58 +0200

    strongly simplify ref module and cuts

--- mod/reference.py
+++ mod/reference.py
@@ -5,10 +5,8 @@
 # --------------------------------------------------------
 
 from src.dut_analysis import DUTAnalysis
-from src.utils import save_pickle
-from plotting.draw import prep_kw, fit_fwhm, remove_low_stat_bins
-from src.dut import Plane
-import src.bins as bins
+from mod.residuals import ResidualAnalysis
+from mod.ref_cuts import RefCut
 
 
 class RefAnalysis(DUTAnalysis):
@@ -19,33 +17,8 @@ class RefAnalysis(DUTAnalysis):
         self.__dict__.update(parent.__dict__)
         self.MetaSubDir = 'REF'
 
-        self.Plane = Plane(self.Config.getint('DUT', 'reference plane'), self.Config('DUT'))
-        self.Data = self.F[str(self.Plane)]
-        self.N = self.Data['Clusters']['X'].size
-
-        from mod.ref_cuts import RefCut
+        self.Plane = self.Planes[self.Config.getint('DUT', 'reference plane')]
+        self.N = self.n
         self.DUTCut = self.Cut
         self.Cut = RefCut(self)
-
-    # ----------------------------------------
-    # region GET
-    def get_du(self, cut=False, **k):
-        return (self.get_data('Clusters', 'U') - self.get_data('Tracks', 'U')[self.Cut['cluster']])[self.Cut(cut)]
-
-    def get_dv(self, cut=False, **k):
-        return (self.get_data('Clusters', 'V') - self.get_data('Tracks', 'V')[self.Cut['cluster']])[self.Cut(cut)]
-
-    @save_pickle('RM')
-    def get_res_means(self):
-        return [fit_fwhm(f(show=False))[1] / 1e3 for f in [self.draw_x_residuals, self.draw_y_residuals]]
-    # endregion GET
-    # ----------------------------------------
-
-    # ----------------------------------------
-    # region DRAW
-    def draw_correlation(self, m='y', cut=True, **dkw):
-        c1, c2 = self.Cut.make_corrs(cut)
-        v1, v2 = self.get_data('Clusters', m.upper()), self.Parent.get_data('Clusters', m.upper(), cut=False)
-        return remove_low_stat_bins(self.Draw.histo_2d(v1[c1], v2[c2], bins.get_corr(m, self.Plane, self.Parent.Plane), **prep_kw(dkw, x_tit=f'{m} REF', y_tit=f'{m} DUT')), .1, of_max=True)
-    # endregion DRAW
-    # ----------------------------------------
+        self.Residuals = ResidualAnalysis(self)
