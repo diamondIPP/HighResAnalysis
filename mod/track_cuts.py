7caacf41f6aec713d2fa5c27ccf8d920685555de
michareichmann <micha.reichmann@gmail.com>
2022-04-07 21:35:31 +0200

    add track mask cut

--- mod/track_cuts.py
+++ mod/track_cuts.py
@@ -4,7 +4,7 @@
 # created on March 30th 2022 by M. Reichmann (remichae@phys.ethz.ch)
 # --------------------------------------------------------
 
-from mod.dut_cuts import DUTCut
+from mod.dut_cuts import DUTCut, zeros, save_hdf5, choose
 
 
 class TrackCut(DUTCut):
@@ -25,8 +25,14 @@ class TrackCut(DUTCut):
         self.register('tp', self.make_trigger_phase(_redo=redo), 10, 'trigger phase')
         self.register('res', self.Ana.REF.Cut.make_trk_residual(redo), 20, 'tracks with a small residual in the REF')
         self.register('fid', self.make_fiducial(_redo=redo), 30, 'tracks in fiducial area')
+        self.register('mask', self.make_mask(_redo=redo), 31, 'masked pixels for tracks')
         self.register('tstart', self.make_start_time(_redo=redo), 40, 'exclude first events')
         self.register('chi2', self.make_chi2(_redo=redo), 50, 'small chi2')
 
     def make_trk(self, trks):
         return self.make_ev(trks, self.Ana.NTracks)
+
+    @save_hdf5('TrackMask', arr=True, dtype='?')
+    def make_mask(self, t=None, _redo=False):
+        return self.make_cluster_mask(*self.get_config('track mask', default=zeros((0, 2))).T, t=choose(t, self.get_config('track mask range', default=1.1)))
+
