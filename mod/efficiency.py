eeb1527d7aad083fd03296e0ee1fb2b144de9b5c
michareichmann <micha.reichmann@gmail.com>
2022-04-07 21:36:43 +0200

    revise efficiency module

--- mod/efficiency.py
+++ mod/efficiency.py
@@ -3,14 +3,15 @@
 #       class for efficiency of a single DUT
 # created on March 22nd 2022 by M. Reichmann (remichae@phys.ethz.ch)
 # --------------------------------------------------------
-from src.dut_analysis import DUTAnalysis, calc_eff, prep_kw, find_bins, bins, get_2d_hist_vec, update_canvas, partial
+from src.dut_analysis import DUTAnalysis, calc_eff, prep_kw, bins, get_2d_hist_vec, update_canvas, partial, mean, ax_range
 
 
 class Efficiency(DUTAnalysis):
 
     def __init__(self, parent):  # noqa
         self.__dict__.update(parent.__dict__)
-        self.MetaSubDir = 'Efficiency'
+        self.MetaSubDir = 'Eff'
+        self.Cut = self.Tracks.Cut
 
     def __call__(self):
         return self.get_value()
@@ -19,8 +20,8 @@ class Efficiency(DUTAnalysis):
     def y_args(self):
         return {'y_tit': 'Efficiency [%]', 'y_range': [0, 105]}
 
-    def get_values(self, trk_cut=None):
-        return (self.get_cluster_size(trk_cut=trk_cut) > 0).astype('u2') * 100
+    def get_values(self, cut=None):
+        return (self.get_cluster_size(cut=cut) > 0).astype('u2') * 100
 
     def get_segment_values(self, nx=2, ny=3, cut=None):
         return get_2d_hist_vec(self.draw_map(local=True, cut=cut, binning=bins.make2d(*self.get_segments(nx, ny)), show=False), err=False, flat=False)
@@ -29,16 +30,16 @@ class Efficiency(DUTAnalysis):
         return calc_eff(values=self.get_values(trk_cut))
 
     def draw(self, bw=None, **dkw):
-        t, e = self.get_time(trk_cut=None), self.get_values() / 100
-        return self.Draw.efficiency(t, e, find_bins(t, w=bw), **prep_kw(dkw, **self.t_args(), **self.y_args, stats=False))
+        t, e = self.get_time(), self.get_values() / 100
+        return self.Draw.efficiency(t, e, **prep_kw(dkw, w=bw, **self.t_args(), **self.y_args, stats=False))
 
     def draw_vs_tp(self, **dkw):
-        x, y = [f(trk_cut=self.Tracks.Cuts.exclude('triggerphase')) for f in [self.get_trigger_phase, self.get_values]]
-        return self.Draw.profile(x, y, bins.TP, 'Efficiency vs. Trigger Phase', **prep_kw(dkw, x_tit='Trigger Phase', **self.y_args))
+        x, y = [f(cut=self.Cut.exclude('tp')) for f in [self.get_trigger_phase, self.get_values]]  # noqa
+        return self.Draw.efficiency(x, y / 100, bins.TP, 'Efficiency vs. Trigger Phase', **prep_kw(dkw, x_tit='Trigger Phase', **self.y_args))
 
     def draw_map(self, res=.5, local=True, eff=True, both=False, fid=False, cut=None, **dkw):
-        (x, y), e = [f(trk_cut=self.Tracks.Cuts.get_nofid(cut, fid)) for f in [partial(self.Tracks.get_xy, local=local), self.get_values]]
-        p = self.Draw.prof2d(x, y, e, **prep_kw(dkw, title='Efficiency Map', binning=bins.get_xy(local, self.Plane, res), **self.get_ax_tits(local), leg=self.get_fid() if eff else None))
+        (x, y), e = [f(cut=self.Cut.get_nofid(cut, fid)) for f in [partial(self.Tracks.get_xy, local=local), self.get_values]]
+        p = self.Draw.prof2d(x, y, e, **prep_kw(dkw, title='Efficiency Map', binning=bins.get_xy(local, self.Plane, res), **self.ax_tits(local)))
         self.draw_text(self.Surface, cut, eff)
         self.draw_text(not self.Surface, cut, eff and both)
         return p
@@ -46,15 +47,14 @@ class Efficiency(DUTAnalysis):
     def draw_text(self, surface, cut, show=True):
         if show:
             self.activate_surface(surface)
-            self.get_fid(show=show)
-            x, y = self.Cuts.get_fid_config(surface)
-            self.Draw.tlatex(x[0] + (x[2] - x[0]) / 2, y[0] + (y[1] - y[0]) / 2, '{:2.1f}%'.format(self.get_value(cut)[0]), 'eff', 22, size=.04)
+            self.Cut.get_fid(surface, show=show)
+            x, y = self.Cut.get_fid_config(surface)
+            self.Draw.textbox(f'{self.get_value(cut)[0]:2.1f}%', mean(ax_range(x)), mean(ax_range(y)), s=2, opacity=.4, ndc=False)
 
     def draw_segments(self, res=.5, local=True, nx=10, ny=15, cut=None, **dkw):
         e = self.get_segment_values(nx, ny, cut)
         self.draw_map(res, local, cut=cut, eff=False, **dkw)
         xs, ys = self.get_segment_centres(nx, ny)
-        print(xs.size, ys.size)
         [self.Draw.tlatex(x, y, '{:2.1f}'.format(e[i][j]), str(i * y.size + j), 22, size=.02) for j, x in enumerate(xs) for i, y in enumerate(ys)]
         self.draw_grid(nx, ny)
 
