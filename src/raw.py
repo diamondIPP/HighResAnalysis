65f2c77d6f51e00ba4bc2cc1a3dca5724b412b96
michareichmann <micha.reichmann@gmail.com>
2022-07-14 09:37:53 +0200

    rewrite

--- src/raw.py
+++ src/raw.py
@@ -7,19 +7,23 @@ from subprocess import check_call
 import h5py
 from numpy import all, array
 
-from plotting.draw import critical, Draw, info, remove_file, Path
+from plotting.draw import critical, Draw, info, remove_file, Path, choose, prep_kw
+from plotting.fit import Gauss
 from src.converter import Converter
+from utility.affine_transformations import transform
+from src.dut import Plane
 
 
 class Raw:
 
-    def __init__(self, c: Converter, load_file=False):
+    def __init__(self, c: Converter, load_file=False, step=-1):
 
         self.Parent = c
         self.RunNumber = c.RunNumber
         self.NT = c.NTelPlanes
         self.ND = c.NDUTPlanes
         self.P = range(self.NT)
+        self.Planes = [Plane(i, c.Config('TELESCOPE' if i < self.NT else 'DUT')) for i in range(self.NT + self.ND)]
 
         self.SoftDir = c.SoftDir.joinpath(c.Config.get('SOFTWARE', 'eudaq2'))
         self.DataDir = c.DataDir
@@ -32,6 +36,7 @@ class Raw:
             self.F = c.F if c.F is not None else h5py.File(c.OutFileName, 'r')
 
         self.Steps = [(self.convert, self.OutFilePath)]
+        self.AtStep = step
         self.Draw = Draw(c.Config.FilePath)
 
     def __repr__(self):
@@ -74,19 +79,60 @@ class Raw:
     def z(self):
         return self.Parent.Proteus.get_z_positions(raw=True)[:self.NT]
 
-    def get_all(self, g, k):
-        return array([self.get(p, g, k)[c] for p, c in zip(self.P, self.cuts())])
+    def l2g(self, x, y, p, step=None):
+        p = self.Planes[p]
+        a = self.Parent.Proteus.get_alignment(choose(step, self.AtStep))['sensors'][p.Number]
+        ox, oy = array(a['offset'][:2])
+        rx, ry = array(a['unit_u']), a['unit_v']
+        return transform(x, y, sx=p.PX, sy=p.PY, ox=ox, oy=oy, rx=rx, ry=ry, order='trs')
 
-    def cuts(self):
+    def xy(self, p, cut=...):
+        return [self.get(p, 'Clusters', k)[cut] for k in ['X', 'Y']]
+
+    def txy(self, p, cut=...):
+        return [self.get(p, 'Tracks', k)[cut] for k in ['X', 'Y']]
+
+    def uv(self, p, cut=..., step=None):
+        return self.l2g(*self.xy(p, cut), p, step)
+
+    def uvs(self, step=None):
+        return array([self.uv(p, c, step) for p, c in zip(self.P, self.cuts())])
+
+    def tuv(self, p, cut=..., step=None):
+        return self.l2g(*self.txy(p, cut), p, step)
+
+    def tuvs(self, step=None):
+        return array([self.tuv(p, self.cut(), step) for p in self.P])
+
+    def du(self, p, step=None):
+        c = self.all_planes()
+        return self.uv(p, self.cuts(c)[p], step)[0] - self.tuv(p, self.cut(c), step)[0]
+
+    def all_planes(self):
+        return array(self.F['Tracks']['Size']) == self.NT
+
+    def cuts(self, cut=True):
         n = [self.get(p, 'Clusters', 'Size') > 0 for p in self.P]
-        c = all(n, axis=0)
+        c = all(n, axis=0) & cut
         return [c[i] for i in n]
 
-    def draw_track_x(self, i=0):
-        self.Draw.graph(self.z(), self.get_all('Clusters', 'U').T[i], x_tit='Z [mm]', y_tit='U [mm]')
+    def res(self):
+        return array([Gauss(self.draw_du(p, show=False)).fit(draw=False).get_pars(err=False)[1:] for p in self.P])
 
-    def draw_track_y(self, i=0):
-        self.Draw.graph(self.z(), self.get_all('Clusters', 'V').T[i], x_tit='Z [mm]', y_tit='V [mm]')
+    def cut(self, cut=True):
+        return all([self.get(p, 'Clusters', 'Size') > 0 for p in self.P], axis=0) & cut
+
+    def draw_clu_u(self, i=0, step=None):
+        self.Draw.graph(self.z(), self.uvs(step)[:, 0, :].T[i], x_tit='Z [mm]', y_tit='Cluster U [mm]')
+
+    def draw_track_u(self, i=0, step=None):
+        self.Draw.graph(self.z(), self.tuvs(step)[:, 0, :].T[i], x_tit='Z [mm]', y_tit='Track U [mm]')
+
+    def draw_du(self, p=0, step=None, **dkw):
+        return self.Draw.distribution(self.du(p, step) * 1e3, **prep_kw(dkw, x_tit='dU [#mum]'))
+
+    def draw_clu_y(self, i=0, step=None):
+        self.Draw.graph(self.z(),  self.uvs(step)[:, 1, :].T[i], x_tit='Z [mm]', y_tit='V [mm]')
     # ----------------------------------------
     # endregion ANALYSIS
 
@@ -94,6 +140,6 @@ class Raw:
 if __name__ == '__main__':
     from src.analysis import Analysis
 
-    a = Analysis()
-    c_ = Converter(a.BeamTest.Path, 11, a.Config)
-    z = Raw(c_, load_file=True)
+    a_ = Analysis()
+    c_ = Converter(a_.BeamTest.Path, 11, a_.Config)
+    z = Raw(c_, load_file=True, step=2)
