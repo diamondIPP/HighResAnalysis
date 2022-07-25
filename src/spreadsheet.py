ae19576fd90d677347fe119bd71379f0765ad59c
michareichmann <micha.reichmann@gmail.com>
2022-07-25 11:50:41 +0200

    add cern 201810 data

--- src/spreadsheet.py
+++ src/spreadsheet.py
@@ -57,28 +57,40 @@ def make_desy_run_log():
     print('successfully extracted data from spreadsheet and saved it as "runlog.json"')
 
 
-def make_cern_run_log():
-    sheet = client.open_by_key('1KoDi9OLU0SiqtLvTGgglQGHm51xn5J0ErAs89h6a-Rc').worksheet('KARTEL')
+info = {'2018-09': {'key': '1KoDi9OLU0SiqtLvTGgglQGHm51xn5J0ErAs89h6a-Rc',
+                    'hv': {'II6-A2': '1-5', 'CMS04': '1-4', 'Si-D7': '1-4'},
+                    'n': [3, 4, 5, 28]},  # [row0 for dut, n dut, n dut rows, row0 for info]
+        '2018-10': {'key': '1t-MXNW0eN9tkGZSakfPdmnd_wcq4cX14Nw0bQ2ma_OQ',
+                    'hv': {'II6-A2': '2-1', 'CMS04': '2-3', 'Si-D8': '2-2', 'II6-B6': '2-3'},
+                    'n': [4, 4, 5, 29]}
+        }
+
+
+def make_cern_run_log(tc='2018-10'):
+    sheet = client.open_by_key(info[tc]['key']).worksheet('KARTEL')
     data = sheet.get_all_values()[1:]
-    hv_supplies = {'II6-A2': '1-5', 'CMS04': '1-4', 'Si-D7': '1-4'}  # sep 2018
-    exclude = ['FEI4']
+    hv = info[tc]['hv']
+    exclude = ['FEI4', '1x5']
     dic = {}
     for row in data:
         run = row[1]
-        if not run or not run.isdigit() or not row[30] or not row[31]:
+        r0, ndut, nrow, r1 = info[tc]['n']
+        if not run or not run.isdigit() or not row[r1 + 1] or not row[r1 + 2] or not row[0].isdigit():
             continue
-        dut_data = array(row[3:23]).reshape((4, -1))  # up to four DUTs
+        dut_data = array(row[r0:r0 + ndut * nrow]).reshape((ndut, -1))  # up to four DUTs
         dut_data = array([[w.strip(' ') for w in lst] for lst in dut_data if lst[0] and not any([w in lst[0] for w in exclude])]).T.tolist()  # remove empty and excluded DUTs
         dut_dict = {name: d for name, d in zip(['duts', 'hv', 'current', 'temp', 'angle'], dut_data)}
+        if not dut_dict:
+            continue
         dic[run] = {'telescope run': int(row[0]),
-                    'start': make_timestamp(row[29], row[30]),
-                    'end': make_timestamp(row[29], row[31]),
-                    'events': int(row[34]) if row[34] else 0,
+                    'start': make_timestamp(row[r1 + 1], row[r1 + 2]),
+                    'end': make_timestamp(row[r1 + 1], row[r1 + 3]),
+                    'events': int(row[r1 + 6]) if row[r1 + 6] else 0,
                     **dut_dict,
-                    'hv supplies': [hv_supplies[dut] if dut in hv_supplies else '' for dut in dut_dict['duts']],
-                    'status': row[33],
-                    'batch': row[28],
-                    'comment': row[36],
+                    'hv supplies': [hv[dut] if dut in hv else '' for dut in dut_dict['duts']],
+                    'status': row[r1 + 5],
+                    'batch': row[r1],
+                    'comment': row[r1 + 8],
                     }
     with open('runlog.json', 'w') as f:
         dump(dic, f, indent=2)
