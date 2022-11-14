3c1eab1f2e700fd4a88f20480d85ca02ee132f32
michareichmann <micha.reichmann@gmail.com>
2022-11-14 09:38:01 +0100

    add fit(), std() and show()

--- mod/residuals.py
+++ mod/residuals.py
@@ -66,9 +66,20 @@ def res_analysis(cls):
 
         # ----------------------------------------
         # region GET
-        @save_pickle('RM', suf_args='all')
-        def means(self, local=False, cut=None, pl=None, _redo=False):
-            return array([Gauss(f(show=False, cut=cut, pl=pl), thresh=.05).fit()[1] for f in ([self.draw_x, self.draw_y] if local else [self.draw_u, self.draw_v])]) / (1 if local else 1e3)
+        @save_pickle('ResFit', suf_args='all')
+        def fit(self, local=False, cut=None, pl=None, _redo=False):
+            return array([Gauss(f(show=False, cut=cut, pl=pl), thresh=.05).fit()[1:] for f in ([self.draw_x, self.draw_y] if local else [self.draw_u, self.draw_v])]) / (1 if local else 1e3)
+
+        def means(self, local=False, cut=None, pl=None, redo=False):
+            return self.fit(local, cut, pl, _redo=redo)[:, 0]
+
+        def std(self, local=False, cut=None, pl=None, redo=False):
+            return self.fit(local, cut, pl, _redo=redo)[:, 1]
+
+        def show(self, redo=False):
+            x, y = self.fit(_redo=redo) * 1e3
+            print(f'Res X: mean = {x[0]:.1f}, std = {x[1]:.1f}')
+            print(f'Res Y: mean = {y[0]:.1f}, std = {y[1]:.1f}')
         # endregion DATA
         # ----------------------------------------
 
