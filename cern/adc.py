1c356a5dffa7dcb0bbc81532d6dafb1215501a27
michareichmann <micha.reichmann@gmail.com>
2023-01-16 12:00:10 +0100

    add option to convert only adc of a given DUT and disregard all others

--- cern/adc.py
+++ cern/adc.py
@@ -7,17 +7,22 @@ import awkward as aw
 import uproot
 from numpy import round
 
-from plotting.utils import info, array
+from plotting.utils import info
 from cern.converter import CERNConverter
+from src.dut import DUT
 from utility.utils import PBAR
 
 
 class Adc2Vcal:
 
-    def __init__(self, c: CERNConverter):
+    def __init__(self, c: CERNConverter, dut_name=None):
+        """Reads the DUT ROOT files saved with pXar and converts adc -> vcal and removes faulty events from the telescope.
+           Specific DUTs can be selected with the <dut_name> arg [default = None -> all DUTs]. """
 
         self.Parent = c
         self.Run = c.Run
+        self.DUTs = [DUT.from_name(n, self.Run.Info) for n in (self.Run.DUTs if dut_name is None else [dut_name])]
+        self.NDUTs = len(self.DUTs)
         self.NTelPlanes = c.NTelPlanes
 
         self.DataDir = c.DataDir.joinpath('dut')
@@ -28,24 +33,23 @@ class Adc2Vcal:
 
         self.Steps = [(self.convert, self.OutFilePath)]
 
-    def check_calibrations(self, n):
-        return all([self.Parent.load_calibration(i).RawFileName.exists() for i in range(n)])
+    def check_calibrations(self):
+        return all([self.Parent.load_calibration(dut.Number).RawFileName.exists() for dut in self.DUTs])
 
     def convert(self):
         """read the DUT raw file and convert the adc values to vcal"""
         CERNConverter.download_raw_file(self.RawFilePath)
-        duts = array(self.Run.Info['duts'])
-        self.check_calibrations(duts.size)
+        self.check_calibrations()
         cut = self.Parent.EventAlignment.cut  # additional events in the DUT data ...
         with uproot.open(self.RawFilePath) as f:
             with uproot.recreate(self.OutFilePath) as g:
                 g['Event'] = f['Event'].arrays()[cut]
-                info(f'converting adc to vcal for DUTs {", ".join(duts)} ...')
-                PBAR.start(duts.size)
-                for i in range(duts.size):
-                    dir_name = f'Plane{self.NTelPlanes + i}'
+                info(f'converting adc to vcal for DUTs {", ".join(dut.Name for dut in self.DUTs)} ...')
+                PBAR.start(self.NDUTs)
+                for i, dut in enumerate(self.DUTs):
+                    dir_name = f'Plane{self.NTelPlanes + dut.Number}'
                     x, y, adc = f[f'{dir_name}/Hits'].arrays(['PixX', 'PixY', 'Value'], library='np').values()
-                    lut = self.Parent.load_calibration(i).get_lookup_table()
+                    lut = self.Parent.load_calibration(dut.Number).get_lookup_table()
                     vcal = [round([lut[lx[i], ly[i], int(lz[i])] for i in range(lx.size)]).astype('i') for lx, ly, lz in zip(x, y, adc)]
                     data = f[f'{dir_name}/Hits'].arrays(filter_name=lambda w: not any([b in w for b in self.FlatBranches]))
                     data['Value'] = aw.values_astype(vcal, 'int32')
